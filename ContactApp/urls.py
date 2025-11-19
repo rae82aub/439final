be1b0b9944bbec9bfef3019f732b3492d0cd1010
danaelchami <danashami22@gmail.com>
2025-11-19 13:01:55 +0200

    fixed login

--- ContactApp/urls.py
+++ ContactApp/urls.py
@@ -3,14 +3,16 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from contacts import views
 
 urlpatterns = [
     path("", include("contacts.urls")),
     path("admin/", admin.site.urls),
 
     # LOGIN / LOGOUT
-    path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(next_page="home"), name="logout"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+
 ]
 
 if settings.DEBUG:
--- contacts/urls.py
+++ contacts/urls.py
@@ -22,8 +22,7 @@ urlpatterns = [
     path("assistant/", views.assistant, name="assistant"),
 
     # AUTH
-    path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(next_page="login"), name="logout"),
+
     path("signup/", views.signup, name="signup"),
 
 ]
