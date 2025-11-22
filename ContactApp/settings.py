81b322476c5bfca46fdcbfe0cdfd19cb7d1effed
rae82aub <rae82@mail.aub.edu>
2025-11-22 18:22:14 +0200

    Update settings.py

--- ContactApp/settings.py
+++ ContactApp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,9 @@ SECRET_KEY = "django-insecure-hl744r6isfa8_4ihqb2a^1(8t1bv%4_o&!g=8mz7zyoaz=i4y1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ["medapp-web.azurewebsites.net"]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
 # Application definition
